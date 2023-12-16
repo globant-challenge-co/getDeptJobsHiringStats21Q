b79e79a19ceac5fb3e7aa77fadb2c1d23eb093d2
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-12-16 00:44:42 -0500

    The method to connect to the database and to query the data was created

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,8 @@ import sys
 import logging
 import json
 import pandas as pd
-import io
+import os
+import psycopg2
 logging.getLogger().setLevel(logging.DEBUG)
 
 def handler(event, context):
@@ -10,9 +11,30 @@ def handler(event, context):
     logging.warning(f'ℹ️🔔' +  sys.version + '!' )
     logging.info(f'ℹ️🔔' +  sys.version + '!' )
     logging.info(f'Body is: {event}')
-    return {'statusCode': 200}
+    queryResult = _getDeptJobsHiringStats21Q()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(queryResult)
+    }
 
 
-# def _getDeptJobsHiringStats21Q():
+def _getDeptJobsHiringStats21Q():
+    conn = psycopg2.connect(
+        dbname=os.environ.get("DB_NAME"), 
+        user=os.environ.get("DB_USER"), 
+        password=os.environ.get("DB_PASSWORD"), 
+        host=os.environ.get("DB_HOST"), 
+        port=os.environ.get("DB_P0RT")
+    )
+    
+    cur = conn.cursor()
+    cur.execute(f'{os.environ.get("DPT_JBS_HI_ST_21Q_VIEW")}')
+    rows = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+    result = [dict(zip(colnames, row)) for row in rows]
+    cur.close()
+    conn.close()
+    return result
+    
     
 
